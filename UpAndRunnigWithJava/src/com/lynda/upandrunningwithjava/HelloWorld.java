614d0cae4f24fd8e7c4c6202d178e761352f7e8f
George Tortocan <george.tortocan@gmail.com>
2015-05-31 14:29:06 +0200

    First applications in Java running from Eclipse and Command Line.

--- UpAndRunnigWithJava/src/com/lynda/upandrunningwithjava/HelloWorld.java
+++ UpAndRunnigWithJava/src/com/lynda/upandrunningwithjava/HelloWorld.java
@@ -1,5 +1,11 @@
 package com.lynda.upandrunningwithjava;
-
+/**
+ * 
+ * @author george
+ *Creating your first application in Java.
+ */
 public class HelloWorld {
-
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ UpAndRunnigWithJava/src/com/lynda/upandrunningwithjava/Main.java
@@ -0,0 +1,29 @@
+package com.lynda.upandrunningwithjava;
+/** 
+ * 
+ * @author george
+ *If you try to run the class from command line because you have an package created an 
+ *its not the default one witch is not recommended by eclipse all cmd should include the
+ *package name.
+ *
+ *First of all you need to be in the "root/src" directory of your project:
+ *"C:\Users\"UserName"\workspace\"ProjectName"\src"
+ *
+ *In case that you implemented in your project an Git repository your default path
+ *to the project will be:
+ *"C:\Users\"UserName"\git\"ProjectName"\src"
+ *
+ *Example:
+ *javac com\lynda\upandrunningwithjava\Main.java for complying your code.
+ *java com\lynda\upandrunningwithjava\Main.java for running your code now you if all works
+ *fine you should get an response from your code in this case the response is an output print of 
+ *Hello from commandline!
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from command line!");
+	}
+
+}
